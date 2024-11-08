6d7e7289415c39d112a13ba23aacb9413bf6153c
Panjun-Kwon <pndlm1979@naver.com>
2024-11-08 12:36:14 +0900

    config : 프로젝트 설정
    - h2 데이터베이스 연결 설정
    - security 기본 설정

---
+++ Backend-Onboarding-Java/src/main/java/com/example/backendonboardingjava/BackendOnboardingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendonboardingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnboardingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendOnboardingJavaApplication.class, args);
+    }
+
+}
---
+++ Backend-Onboarding-Java/src/test/java/com/example/backendonboardingjava/BackendOnboardingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendonboardingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnboardingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
